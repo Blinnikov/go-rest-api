0ca5b5ebda9eab0f4a0f7515eca50a4825309eeb
Igor Blinnikov <igor.blinnikov@gmail.com>
2021-05-23 22:54:59 +0300

    chore(di): store -> sqlstore

--- internal/store/sqlstore/userrepository_test.go
+++ internal/store/sqlstore/userrepository_test.go
@@ -4,14 +4,14 @@ import (
 	"testing"
 
 	"github.com/blinnikov/go-rest-api/internal/app/model"
-	"github.com/blinnikov/go-rest-api/internal/store"
+	"github.com/blinnikov/go-rest-api/internal/store/sqlstore"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestUserRepository_Create(t *testing.T) {
-	db, teardown := store.TestDB(t, databaseURL)
+	db, teardown := sqlstore.TestDB(t, databaseURL)
 	defer teardown("users")
-	s := store.New(db)
+	s := sqlstore.New(db)
 
 	u, err := s.User().Create(model.TestUser(t))
 
@@ -20,9 +20,9 @@ func TestUserRepository_Create(t *testing.T) {
 }
 
 func TestUserRepository_FindByEmail_ReturnsErrorForNoUser(t *testing.T) {
-	db, teardown := store.TestDB(t, databaseURL)
+	db, teardown := sqlstore.TestDB(t, databaseURL)
 	defer teardown("users")
-	s := store.New(db)
+	s := sqlstore.New(db)
 
 	email := "Toto.Cutugno@sanremo.it"
 	_, err := s.User().FindByEmail(email)
@@ -30,9 +30,9 @@ func TestUserRepository_FindByEmail_ReturnsErrorForNoUser(t *testing.T) {
 }
 
 func TestUserRepository_FindByEmail_ReturnsUser(t *testing.T) {
-	db, teardown := store.TestDB(t, databaseURL)
+	db, teardown := sqlstore.TestDB(t, databaseURL)
 	defer teardown("users")
-	s := store.New(db)
+	s := sqlstore.New(db)
 
 	email := "Toto.Cutugno@sanremo.it"
 	tu := model.TestUser(t)
