83aa501392b038d002d28d3d6ee10373b34f9824
Igor Blinnikov <igor.blinnikov@gmail.com>
2021-05-24 00:14:59 +0300

    refactor(server): extract router and logger

--- internal/app/apiserver/apiserver.go
+++ internal/app/apiserver/apiserver.go
@@ -1,72 +1,97 @@
 package apiserver
 
 import (
-	"io"
+	"database/sql"
 	"net/http"
 
-	"github.com/blinnikov/go-rest-api/internal/store"
-	"github.com/gorilla/mux"
-	"github.com/sirupsen/logrus"
+	"github.com/blinnikov/go-rest-api/internal/store/sqlstore"
 )
 
-type APIServer struct {
-	config *Config
-	logger *logrus.Logger
-	router *mux.Router
-	store  *store.Store
-}
+// type APIServer struct {
+// 	config *Config
+// 	logger *logrus.Logger
+// 	router *mux.Router
+// 	store  *store.Store
+// }
 
-func New(config *Config) *APIServer {
-	return &APIServer{
-		config: config,
-		logger: logrus.New(),
-		router: mux.NewRouter(),
-	}
-}
+// func New(config *Config) *APIServer {
+// 	return &APIServer{
+// 		config: config,
+// 		logger: logrus.New(),
+// 		router: mux.NewRouter(),
+// 	}
+// }
 
-func (s *APIServer) Start() error {
-	if err := s.configureLogger(); err != nil {
-		return err
-	}
-
-	s.configureRouter()
-
-	if err := s.configureStore(); err != nil {
-		return err
-	}
-
-	s.logger.Info("starting API server")
-
-	return http.ListenAndServe(s.config.BindAddr, s.router)
-}
-
-func (s *APIServer) configureLogger() error {
-	level, err := logrus.ParseLevel(s.config.LogLevel)
+func Start(config *Config) error {
+	db, err := newDB(config.DatabaseURL)
 	if err != nil {
 		return err
 	}
 
-	s.logger.SetLevel(level)
-	return nil
+	defer db.Close()
+
+	store := sqlstore.New(db)
+	srv := newServer(store)
+
+	return http.ListenAndServe(config.BindAddr, srv)
 }
 
-func (s *APIServer) configureStore() error {
-	st := store.New(s.config.Store)
-
-	if err := st.Open(); st != nil {
-		return err
+func newDB(databaseURL string) (*sql.DB, error) {
+	db, err := sql.Open("postgres", databaseURL)
+	if err != nil {
+		return nil, err
 	}
 
-	s.store = st
-	return nil
-}
-
-func (s *APIServer) configureRouter() {
-	s.router.HandleFunc("/hello", s.handleHello())
-}
-
-func (s *APIServer) handleHello() http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		io.WriteString(w, "Hello, bitch")
+	if err := db.Ping(); err != nil {
+		return nil, err
 	}
+
+	return db, nil
 }
+
+// func (s *APIServer) Start() error {
+// 	if err := s.configureLogger(); err != nil {
+// 		return err
+// 	}
+
+// 	s.configureRouter()
+
+// 	if err := s.configureStore(); err != nil {
+// 		return err
+// 	}
+
+// 	s.logger.Info("starting API server")
+
+// 	return http.ListenAndServe(s.config.BindAddr, s.router)
+// }
+
+// func (s *APIServer) configureLogger() error {
+// 	level, err := logrus.ParseLevel(s.config.LogLevel)
+// 	if err != nil {
+// 		return err
+// 	}
+
+// 	s.logger.SetLevel(level)
+// 	return nil
+// }
+
+// func (s *APIServer) configureStore() error {
+// 	st := store.New(s.config.Store)
+
+// 	if err := st.Open(); st != nil {
+// 		return err
+// 	}
+
+// 	s.store = st
+// 	return nil
+// }
+
+// func (s *APIServer) configureRouter() {
+// 	s.router.HandleFunc("/hello", s.handleHello())
+// }
+
+// func (s *APIServer) handleHello() http.HandlerFunc {
+// 	return func(w http.ResponseWriter, r *http.Request) {
+// 		io.WriteString(w, "Hello, bitch")
+// 	}
+// }
