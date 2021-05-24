e96c8532d66ac96248d8c4b80e689661072f6ef3
Igor Blinnikov <igor.blinnikov@gmail.com>
2021-05-25 01:04:24 +0300

    feat(endpoints): more test for erroneous status codes

--- internal/app/apiserver/server_internal_test.go
+++ internal/app/apiserver/server_internal_test.go
@@ -26,6 +26,18 @@ func TestServer_HandleUsersCreate(t *testing.T) {
 			},
 			expectedCode: http.StatusCreated,
 		},
+		{
+			name:         "invalid payload",
+			payload:      "mess",
+			expectedCode: http.StatusBadRequest,
+		},
+		{
+			name: "invalid params",
+			payload: map[string]string{
+				"email": "email",
+			},
+			expectedCode: http.StatusUnprocessableEntity,
+		},
 	}
 
 	for _, tc := range testCases {
